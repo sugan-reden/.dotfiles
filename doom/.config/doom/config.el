01ede18a5b98c3541b11bedddf832b3c88c46415
Sugan Reden <sugan-reden@pm.me>
2026-02-17 13:24:03 -0500

    doom: add ledger to config and make journal files visible by ledger-mode

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -79,3 +79,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; CUSTOM
+(add-to-list 'auto-mode-alist '("\\.journal\\'" . ledger-mode))
